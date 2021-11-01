d2147894e84699c0565b8181ade09235030fe76b
MarcinParda <marcin98parda@gmail.com>
2021-11-01 22:00:31 +0100

    add icons to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,20 @@
+import {BsFillLightningFill, BsPlus, FaFire, FaPoo} from "react-icons/all";
+
 const Sidebar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+            <SidebarIcon icon={<FaFire size="28" />} />
+            <SidebarIcon icon={<BsPlus size="28" />} />
+            <SidebarIcon icon={<BsFillLightningFill size="28" />} />
+            <SidebarIcon icon={<FaPoo size="28" />} />
+        </div>
+    );
+};
+
+const SidebarIcon = ({ icon }) => {
+    return(
+        <div className="sidebar-icon">
+            {icon}
         </div>
     )
 }
